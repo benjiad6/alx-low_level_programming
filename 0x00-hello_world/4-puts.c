5817b990ad9f339fe58aecd2587b2c69386560e1
benjiad6 <benjiad6@gmail.com>
2023-08-10 18:37:31 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
-*main - Entry pooint
+* main - Entry point
 *
 *Return: Always 0 (success)
 */
 int main(void)
 {
-	puts("\"Programming is like building a mmultilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
