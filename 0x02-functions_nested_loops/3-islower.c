3b6401ff9a8f46bb09cd822ee7333c7d470ebe0e
benjiad6 <benjiad6@gmail.com>
2023-08-15 18:23:51 -0700

    fourth project

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _islower - cheeks for lowercase character
+ * @c: The character to be checked
+ 8 Return: 1 for lowercase character or 0 for anything else
+*/
+
+int _islower(int c)
+
+{
+	if (c >= 97 && c <= 122)
+	{
+
+	return (1);
+	}
+	return (0);
+}
