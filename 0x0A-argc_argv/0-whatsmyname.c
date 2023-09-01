7ea19e79a405ee3552437c6d5a535600f6ff2cc6
benjiad6 <benjiad6@gmail.com>
2023-09-01 14:03:59 -0700

    Start commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Argument count
+ * @argv: Argument vector (array of strings)
+ *
+ * Description: Prints the name of the program.
+ *
+ * Return: Always 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc > 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+	return (0);
+}
